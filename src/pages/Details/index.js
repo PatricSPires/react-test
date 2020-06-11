7f2113864527b96b4825b7dfcaf890dd98ccbadf
PatricSPires <patric.pires@outlook.com>
2020-06-11 15:28:29 -0300

    feat(details): create page

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,9 +1,81 @@
-import React from "react";
+import React, { useState, useEffect, memo } from "react";
+import { useParams } from "react-router-dom";
+import Layout from "../../components/Layout";
+import api from "../../services/api";
 
-// import { Container } from './styles';
+const Details = memo(({ match }) => {
+  const [data, setData] = useState({});
+  const [informations, setInformations] = useState([]);
+  const { id } = useParams();
 
-function Details() {
-  return <div />;
-}
+  useEffect(() => {
+    api.get(`getJSON?geonameId=${id}&username=ptricspires`).then((response) => {
+      const {
+        data: {
+          toponymName,
+          countryCode,
+          name,
+          population,
+          lat,
+          lng,
+          wikipediaURL,
+          timezone: { gmtOffset, timeZoneId },
+        },
+      } = response;
+
+      setData({
+        toponymName,
+        countryCode,
+      });
+
+      setInformations([
+        { key: "Name", value: name },
+        { key: "Time zone", value: timeZoneId },
+        { key: "GMT OFFSET", value: gmtOffset },
+        { key: "Population", value: population },
+        { key: "Latitude", value: lat },
+        { key: "Longitude", value: lng },
+        { key: "Wikipedia", value: wikipediaURL },
+      ]);
+    });
+  }, [id]);
+
+  return (
+    <Layout>
+      {informations && (
+        <div className="card">
+          <div className="card-header">
+            {data.toponymName} - {data.countryCode}
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">{data.name}</h5>
+
+            {informations
+              .filter((info) => info.value)
+              .map((info) => {
+                if (info.key === "Wikipedia") {
+                  return (
+                    <a
+                      href={`https://${info.value}`}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      className="btn btn-outline-info"
+                    >
+                      {info.key}
+                    </a>
+                  );
+                }
+                return (
+                  <p className="card-text">
+                    {info.key}: <span className="text-muted">{info.value}</span>
+                  </p>
+                );
+              })}
+          </div>
+        </div>
+      )}
+    </Layout>
+  );
+});
 
 export default Details;
