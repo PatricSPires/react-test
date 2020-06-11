d3c2aa5265e65c7bd068b466840e89d274e5ddc5
PatricSPires <patric.pires@outlook.com>
2020-06-11 11:38:20 -0300

    feat(home): create home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,29 @@
 import React from "react";
-
-// import { Container } from './styles';
+import Layout from "../../components/Layout";
 
 function Home() {
-  return <div />;
+  return (
+    <Layout>
+      <form action="">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control mb-2"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Cidade, Estado ou País"
+              ></input>
+              <button type="submit" className="btn btn-primary">
+                Buscar
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </Layout>
+  );
 }
 
 export default Home;
