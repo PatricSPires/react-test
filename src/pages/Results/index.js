c1e7192635b4b2bf7a052505f439cb65e002a34e
PatricSPires <patric.pires@outlook.com>
2020-06-11 15:29:19 -0300

    feat(results): use data to details

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,37 +1,38 @@
 import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 import Layout from "../../components/Layout";
 import { BsArrowRight } from "react-icons/bs";
 import api from "../../services/api";
 
-function Results({ match }) {
+function Results() {
   const [results, setResults] = useState();
-
-  const { cep } = match.params;
+  const { search } = useParams();
 
   useEffect(() => {
     api
-      .get(`searchJSON?name=brasil&username=ptricspires&style=Long`)
+      .get(`searchJSON?name=${search}&username=ptricspires&style=Long`)
       .then((response) => setResults(response.data.geonames));
-  }, [cep]);
+  }, [search]);
 
   return (
     <Layout>
       <h2 className="mb-3">Resultados</h2>
 
       <ul className="list-group list-group-flush">
-        {results &&
-          results.map((cep) => (
-            <li
-              key={cep.countryId}
-              className="list-group-item d-flex justify-content-between"
-            >
-              {cep.name}
-              <div className="d-flex justify-content-between align-items-center">
-                <span className="mr-2">{cep.countryCode}</span>
+        {results?.map((cep) => (
+          <li
+            key={cep.countryId}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {cep.name}
+            <div className="d-flex justify-content-between align-items-center">
+              <span className="mr-2">{cep.countryCode}</span>
+              <Link to={`/details/${cep.geonameId}`} className="text-muted">
                 <BsArrowRight />
-              </div>
-            </li>
-          ))}
+              </Link>
+            </div>
+          </li>
+        ))}
       </ul>
     </Layout>
   );
