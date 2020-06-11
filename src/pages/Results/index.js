d1b1b2c46b2d30b4bc2dd259485524e9562268e7
PatricSPires <patric.pires@outlook.com>
2020-06-11 12:37:32 -0300

    feat(results): create page

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/Layout";
+import { BsArrowRight } from "react-icons/bs";
+import api from "../../services/api";
 
-// import { Container } from './styles';
+function Results({ match }) {
+  const [results, setResults] = useState();
 
-function Results() {
-  return <div />;
+  const { cep } = match.params;
+
+  useEffect(() => {
+    api
+      .get(`searchJSON?name=brasil&username=ptricspires&style=Long`)
+      .then((response) => setResults(response.data.geonames));
+  }, [cep]);
+
+  return (
+    <Layout>
+      <h2 className="mb-3">Resultados</h2>
+
+      <ul className="list-group list-group-flush">
+        {results &&
+          results.map((cep) => (
+            <li
+              key={cep.countryId}
+              className="list-group-item d-flex justify-content-between"
+            >
+              {cep.name}
+              <div className="d-flex justify-content-between align-items-center">
+                <span className="mr-2">{cep.countryCode}</span>
+                <BsArrowRight />
+              </div>
+            </li>
+          ))}
+      </ul>
+    </Layout>
+  );
 }
 
 export default Results;
